949b576d8e9cfc9a0c8e4b687633085e274b82e9
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-22 09:54:53 +0530

    Add files via upload
    cpp program to Add two distances in inch-feet by creating a class named 'AddDistance'

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+class Adddistance
+{
+  public:
+  int inch1;
+  int feet1;
+  int inch2;
+  int feet2;
+  int feets;
+  int inches;
+
+ void setData()
+    {
+        cout<<"Enter feets :";
+        cin>>feet1;
+        cout<<"Enter inches :";
+        cin>>inch1;
+        cout<<"Enter feets :";
+        cin>>feet2;
+        cout<<"Enter inches :";
+        cin>>inch2;
+
+    }
+    void getData()
+    {
+        inches=inch1+inch2;
+        feets=feet1+feet2;
+        if(inches>=12)
+            inches/=12;
+        cout<<"Distance : "<<feets<<" ft "<<inches<<" in";
+    }
+    };
+int main()
+{
+   Adddistance d;
+   d.setData();
+   d.getData();
+
+    return 0;
+}
