168cfe066d151fec10261189ef3198c59179bdb5
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-03-01 11:30:23 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+class Programming
+{
+public:
+    string var;
+Programming()
+{
+    cout<<"I LOVE PROGRAMMING LANGUAGES"<<endl;
+}
+Programming(string v)
+{
+    var=v;
+}
+getData()
+{
+    cout<<"I Love "<<var<<endl;
+}
+};
+int main()
+{
+    Programming p;
+    Programming p1("Asterisk");
+    p1.getData();
+    return 0;
+}
