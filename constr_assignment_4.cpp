56c2fe137423c858b713944e2d33c2e4c891fbd9
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-03-22 09:41:05 +0530

    Add files via upload

---
+++ constr_assignment_4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+class AddAmount
+{
+public:
+    int amount=50;
+    AddAmount()
+    {
+        amount=0;
+    }
+    AddAmount(int amt)
+    {
+        amount=amt;
+    }
+    getData()
+    {
+        cout<<"FINAL AMOUNT :"<<amount<<endl;
+    }
+};
+int main()
+{
+    AddAmount b;
+    b.getData();
+    AddAmount b1(50);
+    b1.getData();
+
+
+
+    return 0;
+}
