373bc025276dd51143f2e024fa4e0f19c8858bb2
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-03-24 11:22:05 +0530

    Update constr_assignment_8.cpp

--- constr_assignment_8.cpp
+++ constr_assignment_8.cpp
@@ -1,5 +1,7 @@
 
-/*   */
+/* create a class to print the area of a square and a rectangle.The class has two functions with the same name but different numbers of parameters
+The function for printing the area of rectangle has two parameters which are its length and breadth respectively while the other function for printing 
+the area of square has one parameter which is the side of square*/
 
 #include <iostream>
 
