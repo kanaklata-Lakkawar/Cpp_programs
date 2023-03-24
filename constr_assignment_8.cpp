3263b580c9147aab9e6e7108d6e790ec03af15ea
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-03-24 11:13:23 +0530

    Add files via upload

---
+++ constr_assignment_8.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+class area
+{
+public:
+    int length;
+    int breadth;
+    int side;
+    area()
+    {
+
+    }
+   area(int l,int b)
+   {
+     length=l;
+     breadth=b;
+   }
+   area(int s)
+   {
+       side=s;
+   }
+   void getData()
+   {
+       cout<<"AREA OF RECTANGLE :"<<length*breadth<<endl;
+   }
+};
+int main()
+{
+    area a,a1;
+    a.getData();
+    a1.getData();
+    return 0;
+}
