edc9779177c1fc80925f195df7c7591dd0179a41
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-04-10 18:50:42 +0530

    Add files via upload

---
+++ RuntimePolymorphism.cpp
@@ -0,0 +1,32 @@
+// C++ program to demonstrate function overriding
+
+#include <iostream>
+using namespace std;
+
+class Base
+{
+   public:
+    virtual void print()
+    {
+        cout << "Base Function" << endl;
+    }
+};
+
+class Derived : public Base
+ {
+   public:
+    void print()
+    {
+        cout << "Derived Function" << endl;
+    }
+};
+
+int main()
+{
+    Derived derived1;
+
+
+    derived1.print();
+
+    return 0;
+}
